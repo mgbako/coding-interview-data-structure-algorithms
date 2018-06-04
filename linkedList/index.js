9cecfa8423775a3b21402733ebdc6c0151dfbe73
MgbakoCentric <jmgbako@gmail.com>
2018-06-04 17:33:00 +0100

    first init

---
+++ linkedList/index.js
@@ -0,0 +1,148 @@
+class Node {
+  constructor(data, next = null) {
+    this.data = data;
+    this.next = next;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+
+  insertFirst(data) {
+    this.head = new Node(data, this.head)
+  }
+
+  size() {
+    let counter = 0;
+    let node = this.head;
+
+    while (node) {
+      counter++;
+      node = node.next;
+    }
+
+    return counter;
+  }
+
+  getFirst() {
+    return this.head;
+  }
+
+  getLast() {
+    if (!this.head) {
+      return null;
+    }
+
+    let node = this.head;
+
+    while (node) {
+      if (!node.next) {
+        return node;
+      }
+
+      node = node.next;
+    }
+
+    return node;
+  }
+
+  clear() {
+    this.head = null
+  }
+
+  removeFirst() {
+    if (!this.head) {
+      return;
+    }
+
+    this.head = this.head.next;
+  }
+
+  removeLast() {
+    if (!this.head) {
+      return;
+    }
+
+    if (!this.head.next) {
+      this.head = null;
+      return;
+    }
+
+    let previous = this.head;
+    let node = this.head.next;
+
+    while (node.next) {
+      previous = node;
+      node = node.next;
+    }
+
+    previous.next = null;
+  }
+
+  insertLast(data) {
+    const last = this.getLast();
+    const newNode = new Node(data);
+
+    if (last) {
+      last.next = newNode;
+    } else {
+      this.head = newNode;
+    }
+
+  }
+
+  getAt(index) {
+    let counter = 0;
+    let node = this.head;
+    while (node) {
+      if (counter === index) {
+        return node;
+      }
+
+      counter++;
+      node = node.next;
+    }
+    return null;
+  }
+
+  removeAt(index) {
+    if (!this.head) {
+      return;
+    }
+
+    if (index === 0) {
+      this.head = this.head.next;
+      return;
+    }
+
+    const previous = this.getAt(index - 1);
+    if (!previous || !previous.next) {
+      return;
+    }
+    previous.next = previous.next.next;
+  }
+
+  insertAt(data, index) {
+    if (!this.head) {
+      this.head = new Node(data);
+      return;
+    }
+
+    if (index === 0) {
+      this.head = new Node(data, this.head);
+      return;
+    }
+
+    const previous = this.getAt(index - 1) || this.getLast();
+    const node = new Node(data, previous.next);
+    previous.next = node;
+  }
+}
+
+
+module.exports = {
+  Node,
+  LinkedList
+}
---
+++ linkedList/test.js
@@ -0,0 +1,164 @@
+const List = require('./index.js').LinkedList;
+const expect = require('chai').expect;
+
+
+describe("GetLast", () => {
+  it('Should return the last element', () => {
+    const l = new List();
+    l.insertFirst(2);
+    expect(l.getLast()).to.eql({
+      data: 2,
+      next: null
+    });
+    l.insertFirst(1);
+    expect(l.getLast()).to.eql({
+      data: 2,
+      next: null
+    })
+  })
+});
+
+describe('Clear', () => {
+  it('Should empty out the list', () => {
+    const l = new List();
+    expect(l.size()).to.eql(0)
+    l.insertFirst(1);
+    l.insertFirst(1);
+    l.insertFirst(1);
+    l.insertFirst(1);
+    expect(l.size()).to.eql(4)
+    l.clear();
+    expect(l.size()).to.eql(0);
+  })
+});
+
+describe('Remove First', () => {
+  it('Should remove the first Element from the list', () => {
+    const l = new List();
+    l.insertFirst('a');
+    l.removeFirst();
+    expect(l.size()).to.eql(0)
+    expect(l.getFirst()).to.be.null;
+  });
+});
+
+describe('RemoveLast', () => {
+  it('Should Remove the last element in the list', () => {
+    const l = new List();
+    l.insertFirst('a');
+    l.insertFirst('b');
+
+
+    l.removeLast();
+
+    expect(l.size()).to.eql(1);
+    expect(l.head.data).to.eql('b')
+  });
+});
+
+describe('InsertLast', () => {
+  it('Should add element to the end of the list', () => {
+    const l = new List();
+    l.insertFirst('a');
+    l.insertFirst('b');
+
+
+    l.insertLast('c');
+
+    expect(l.size()).to.eql(3);
+    expect(l.getLast().data).to.eql('c')
+  });
+});
+
+describe('Insert 1 element at the Last', () => {
+  it('Should add 1 element to the end of the list', () => {
+    const l = new List();
+
+    l.insertLast('d');
+
+    expect(l.size()).to.eql(1);
+    expect(l.getLast().data).to.eql('d')
+  });
+});
+
+describe('Get At', () => {
+  it('Should return the node at a given index', () => {
+    const l = new List();
+    expect(l.getLast(10)).to.be.null;
+
+    l.insertLast(1);
+    l.insertLast(2);
+    l.insertLast(3);
+    l.insertLast(4);
+
+    expect(l.getAt(0).data).to.eql(1);
+    expect(l.getAt(1).data).to.eql(2);
+    expect(l.getAt(2).data).to.eql(3);
+  });
+});
+
+describe('Remove At', () => {
+  it('Should remove and not crash', () => {
+    const l = new List();
+    expect(() => {
+      l.removeAt(0);
+      l.removeAt(1);
+      l.removeAt(2);
+    }).not.to.Throw();
+  });
+
+  it('Should remove at an index and not crash', () => {
+    const l = new List();
+    expect(() => {
+      const l = new List();
+      l.insertFirst('a');
+      l.removeAt(1);
+    }).not.to.Throw();
+  });
+
+  it('Should remove the first node', () => {
+    const l = new List();
+    l.insertLast(1);
+    l.insertLast(2);
+    l.insertLast(3);
+    l.insertLast(4);
+    expect(l.getAt(0).data).to.eql(1);
+    l.removeAt(0);
+    expect(l.getAt(0).data).to.eql(2);
+  });
+});
+
+describe('Insert At', () => {
+  it('Should insert a new node with data at 0 index when the list is empty', () => {
+    const l = new List();
+    l.insertAt('hi', 0),
+      expect(l.getFirst().data).to.eql('hi');
+  });
+
+  it('Should insert a new node with data at 0 index when the list is not empty', () => {
+    const l = new List();
+    l.insertLast('a');
+    l.insertLast('b');
+    l.insertLast('c');
+    l.insertAt('hi', 0);
+    expect(l.getAt(0).data).to.eql('hi');
+    expect(l.getAt(1).data).to.eql('a');
+    expect(l.getAt(2).data).to.eql('b');
+    expect(l.getAt(3).data).to.eql('c');
+  });
+
+  it('Should insert a new node with data at a middle index', () => {
+    const l = new List();
+    l.insertFirst('a');
+    l.insertLast('b');
+    l.insertLast('c');
+    l.insertLast('d');
+    l.insertAt('hi', 2);
+
+    expect(l.getAt(0).data).to.eql('a');
+    expect(l.getAt(1).data).to.eql('b');
+    expect(l.getAt(2).data).to.eql('hi');
+    expect(l.getAt(3).data).to.eql('c');
+    expect(l.getAt(4).data).to.eql('d');
+  });
+});
